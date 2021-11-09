7832a1a33b226b368a08fbbba2b9f543ca167c79
yusian-du <n4563810@gmail.com>
2021-11-09 17:21:35 +0800

    add signup page

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 import SignIn from '../views/SignIn'
+
 import NotFound from '../views/NotFound'
 
 Vue.use(VueRouter)
@@ -10,6 +11,11 @@ const routes = [
     path: '/signin',
     name: 'sign-in',
     component: SignIn
+  },
+  {
+    path: '/signup',
+    name: 'sign-up',
+    component: () => import ('../views/SignUp')
   },
   {
     path: '*',
