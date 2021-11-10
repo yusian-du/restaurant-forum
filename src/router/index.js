07032e85b3fe3e6b8aa94a4abb8a67f6a1a3e2d1
yusian-du <n4563810@gmail.com>
2021-11-10 13:49:44 +0800

    add main page

--- src/router/index.js
+++ src/router/index.js
@@ -1,12 +1,17 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 import SignIn from '../views/SignIn'
-
+import Restaurants from '../views/Restaurants.vue'
 import NotFound from '../views/NotFound'
 
 Vue.use(VueRouter)
 
 const routes = [
+  {
+    path: '/',
+    name: 'root',
+    redirect: '/restaurants'
+  },
   {
     path: '/signin',
     name: 'sign-in',
@@ -16,6 +21,26 @@ const routes = [
     path: '/signup',
     name: 'sign-up',
     component: () => import ('../views/SignUp')
+  },
+  {
+    path: '/restaurants',
+    name: 'restaurants',
+    component: Restaurants
+  },
+  {
+    path: '/restaurants/feeds',
+    name: 'restaurants-feeds',
+    component: () => import ('../views/RestaurantsFeeds')
+  },
+  {
+    path: '/restaurants/top',
+    name: 'restaurants-tops',
+    component: () => import ('../views/RestaurantsTop')
+  },
+  {
+    path: '/users/top',
+    name: 'users-top',
+    component: () => import ('../views/UsersTop')
   },
   {
     path: '*',
