6c5df6423783b107b0829c8bb8728185755bfcea
yusian-du <n4563810@gmail.com>
2021-11-24 11:51:01 +0800

    fix: RestaurantsTop 串接 API

--- src/apis/restaurants.js
+++ src/apis/restaurants.js
@@ -8,6 +8,11 @@ export default {
             headers: { Authorization: `Bearer ${getToken()}` }
         })
     },
+    getTopRestaurants () {
+        return apiHelper.get(`/restaurants/top`, {
+            headers: { Authorization: `Bearer ${getToken()}` }
+        })
+    },
     getFeeds () {
         return apiHelper.get('/restaurants/feeds', {
             headers: { Authorization: `Bearer ${getToken()}` }
