d5568e419149ab6dc941ee6d3c62ac2a30a95577
yusian-du <n4563810@gmail.com>
2021-11-25 20:21:21 +0800

    fix: SignUp 串接 API

--- src/views/SignUp.vue
+++ src/views/SignUp.vue
@@ -87,6 +87,9 @@
 </template>
 
 <script>
+import authorizationAPI from './../apis/authorization'
+import { Toast } from './../utils/helpers'
+
 export default {
     data () {
         return {
@@ -97,15 +100,53 @@ export default {
         }
     },
     methods: {
-        handleSubmit () {
-            const data = JSON.stringify({
-                name: this.name,
-                email: this.email,
-                password: this.password,
-                passwordCheck: this.passwordCheck
+        async handleSubmit () {
+          try {
+            if (
+              !this.name ||
+              !this.email ||
+              !this.password ||
+              !this.passwordCheck
+            ) {
+              Toast.fire({
+                icon: 'warning',
+                title: '請確認已填寫所有欄位'
+              })
+              return
+            }
+
+            if (this.password !== this.passwordCheck) {
+              Toast.fire({
+                icon: 'warning',
+                title: '兩次輸入的密碼不同'
+              })
+              this.passwordCheck = ''
+              return
+            }
+
+            const { data } = await authorizationAPI.signUp({
+              name: this.name,
+              email: this.email,
+              password: this.password,
+              passwordCheck: this.passwordCheck
             })
 
-            console.log('data', data)
+            if (data.staus === 'error') {
+              throw new Error(data.message)
+            }
+
+            Toast.fire({
+              icon: 'success',
+              title: data.message
+            })
+
+            this.$router.push('/signin')
+          } catch (error) {
+            Toast.fire({
+              icon: 'warning',
+              title: `無法註冊 - ${error.message}`
+            })
+          }
         }
     }
 }
