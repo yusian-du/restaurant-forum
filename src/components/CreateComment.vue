70414fde94aacfe7782d00cc7d635a6fcda5af4f
yusian-du <n4563810@gmail.com>
2021-11-29 11:20:28 +0800

    Add stylesheets for the restaurant forum

--- src/components/CreateComment.vue
+++ src/components/CreateComment.vue
@@ -83,3 +83,9 @@ export default {
     }
 }
 </script>
+
+<style scoped>
+.form-group {
+  margin: 21px 0 8px;
+}
+</style>
--- src/components/NavTabs.vue
+++ src/components/NavTabs.vue
@@ -17,32 +17,46 @@
 <script>
 import {v4 as uuidv4} from 'uuid'
 export default {
-    data () {
-        return {
-            tabs: [
-              {
-                id: uuidv4(),
-                title: '首頁',
-                path: '/restaurants'
-              },
-              {
-                id: uuidv4(),
-                title: '最新動態',
-                path: '/restaurants/feeds'
-              },
-              {
-                id: uuidv4(),
-                title: 'TOP 10 人氣餐廳',
-                path: '/restaurants/top'
-              },
-              {
-                id: uuidv4(),
-                title: '美食達人',
-                path: '/users/top'
-              },
-                
-            ]
-        }
-     }
+  data () {
+    return {
+      tabs: [
+       {
+        id: uuidv4(),
+        title: '首頁',
+        path: '/restaurants'
+       },
+      {
+        id: uuidv4(),
+        title: '最新動態',
+        path: '/restaurants/feeds'
+      },
+      {
+        id: uuidv4(),
+        title: 'TOP 10 人氣餐廳',
+        path: '/restaurants/top'
+      },
+      {
+        id: uuidv4(),
+        title: '美食達人',
+        path: '/users/top'
+      },
+    ]
+  }
+ }
 }
 </script>
+
+<style scoped>
+  .nav-tabs {
+    border-bottom: 1px solid #bd2333;
+  }
+
+  .nav-tabs .nav-link:focus,
+  .nav-tabs .nav-link:hover,
+  .nav-tabs .nav-item.show .nav-link,
+  .nav-tabs .nav-link.active {
+    border-color: #bd2333;
+    background-color: #bd2333;
+    color: white;
+}
+</style>
